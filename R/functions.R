9f6740fe0ba9a14817e0d506b68f52d6a6950717
Jon <test>
2021-04-30 18:00:57 +0200

    Added a function to plot convergence of the algorithm

--- R/functions.R
+++ R/functions.R
@@ -15,3 +15,30 @@ get_deviance <- function(beta, x, y, family) {
   mu <- family$linkinv(x %*% beta)
   sum(family$dev.resids(y, mu, rep(1,nrow(x))))
 }
+
+#' Create a plot to examine the convergence of the algorithm.
+#'
+#' @param result The results from irls.sgd.
+#' @param true_coeffs A vector of true coefficients if they are available.
+#' @param x A matrix of independent variables to estimate the true coefficients from.
+#' @param y A vector with the dependent variable to estimate the true coefficients from.
+#' @param family A glm family for the distribution to use, i.e. "binomial()" when estimating the true coefficients.
+#'
+#' @export conv.plot
+conv.plot <- function (result, true_coeffs=NULL, x=NULL, y=NULL, family=binomial()) {
+  if (is.null(true_coeffs) & !is.null(x) & !is.null(y)) {
+    cat("Estimating full model...\n")
+    true_coeffs <- glm.fit(x, y, family=gaussian())$coefficients
+  }
+  for (i in 1:ncol(result$irls_hist)) {
+    if (!is.null(true_coeffs)) {
+      multiplot(cbind(c(result$irls_hist[,i], result$sgd_hist[,i]), true_coeffs[i]),
+                frame.plot=F, ylab=bquote(beta[.(i-1)]), xlab="Iteration")
+    }
+    else {
+      multiplot(c(result$irls_hist[,i], result$sgd_hist[,i]),
+                frame.plot=F, ylab=bquote(beta[.(i-1)]), xlab="Iteration")
+    }
+    abline(v=length(result$irls_hist[,i]))
+  }
+}
