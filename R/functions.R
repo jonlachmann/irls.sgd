99e3f65027783f22c05298f83f1deb59336a058d
Jon <test>
2021-04-30 18:01:55 +0200

    Added the multiplot function which the previous depends on

--- R/functions.R
+++ R/functions.R
@@ -42,3 +42,19 @@ conv.plot <- function (result, true_coeffs=NULL, x=NULL, y=NULL, family=binomial
     abline(v=length(result$irls_hist[,i]))
   }
 }
+
+# Plot many columns in a matrix, log scale can be enabled too
+multiplot <- function (mat, logscale=F, ylim=c(min(mat), max(mat)), legend=F, names=names(mat), ...) {
+  if (logscale) {
+    mat[mat > 0] <- log(mat[mat > 0])
+    mat[mat < 0] <- -log(-mat[mat < 0])
+  }
+  mat <- as.matrix(mat)
+  rbcol <- rainbow(ncol(mat))
+  plot(mat[,1], type="l", ylim=ylim, col=rbcol[1], ...)
+  if (ncol(mat) > 1) for (i in 2:ncol(mat)) lines(mat[,i], col=rbcol[i])
+  if (legend) {
+    if (is.null(names)) names <- 1:ncol(mat)
+    legend("bottomright", col=rbcol, legend=names, lty=1)
+  }
+}
